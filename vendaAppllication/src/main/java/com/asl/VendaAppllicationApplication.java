cb1aa700a1b68539ffa61bc643a1d093782c0e85
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2023-12-08 23:09:54 -0300

    createApplication

---
+++ vendaAppllication/src/main/java/com/asl/VendaAppllicationApplication.java
@@ -0,0 +1,13 @@
+package com.asl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaAppllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaAppllicationApplication.class, args);
+	}
+
+}
